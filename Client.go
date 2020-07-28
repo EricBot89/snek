a1110ea190cd8ebb7c5f5412a0e102921b0c677f
EricBot89 <eloucks@gmail.com>
2020-07-28 09:11:35 -0400

    use one tcp connection for each client

--- Client.go
+++ Client.go
@@ -18,6 +18,7 @@ type SnekClient struct {
 	ip   string
 	port string
 	game GameData
+	conn *bufio.ReadWriter
 }
 
 //NewClient client constructor
@@ -29,6 +30,15 @@ func NewClient(name string, ip string, port string) SnekClient {
 	}
 }
 
+//Open a tcp connection to snek server
+func Open(addr string) (*bufio.ReadWriter, error) {
+	conn, err := net.Dial("tcp", addr)
+	if err != nil {
+		return nil, err
+	}
+	return bufio.NewReadWriter(bufio.NewReader(conn), bufio.NewWriter(conn)), nil
+}
+
 func (client *SnekClient) joinServer() error {
 	rw, openErr := Open(client.ip + client.port)
 	if openErr != nil {
@@ -36,17 +46,19 @@ func (client *SnekClient) joinServer() error {
 		return openErr
 	}
 
-	_, writeErr := rw.WriteString("JOIN\n")
+	client.conn = rw
+
+	_, writeErr := client.conn.WriteString("JOIN\n")
 	if writeErr != nil {
 		log.Println("Failed to write to stream", writeErr)
 		return writeErr
 	}
-	_, writeErr = rw.WriteString(client.name + "\n")
+	_, writeErr = client.conn.WriteString(client.name + "\n")
 	if writeErr != nil {
 		log.Println("Failed to write to stream", writeErr)
 		return writeErr
 	}
-	flushErr := rw.Flush()
+	flushErr := client.conn.Flush()
 	if flushErr != nil {
 		log.Println("Failed to flush", flushErr)
 		return flushErr
@@ -64,28 +76,22 @@ func (client *SnekClient) joinServer() error {
 }
 
 func (client *SnekClient) sendKey(key termbox.Event) error {
-	rw, openErr := Open(client.ip + client.port)
-	if openErr != nil {
-		log.Println("Failed to connect", openErr)
-		return openErr
-	}
-
-	_, writeErr := rw.WriteString("KEY\n")
+	_, writeErr := client.conn.WriteString("KEY\n")
 	if writeErr != nil {
 		log.Println("Failed to write to stream", writeErr)
 		return writeErr
 	}
-	_, writeErr = rw.WriteString(client.name + "\n")
+	_, writeErr = client.conn.WriteString(client.name + "\n")
 	if writeErr != nil {
 		log.Println("Failed to write to stream", writeErr)
 		return writeErr
 	}
-	enc := gob.NewEncoder(rw)
+	enc := gob.NewEncoder(client.conn)
 	err := enc.Encode(key)
 	if err != nil {
 		log.Println("Failed to encode termbox event")
 	}
-	flushErr := rw.Flush()
+	flushErr := client.conn.Flush()
 	if flushErr != nil {
 		log.Println("Failed to flush", flushErr)
 		return flushErr
@@ -94,28 +100,22 @@ func (client *SnekClient) sendKey(key termbox.Event) error {
 }
 
 func (client *SnekClient) requestGame() error {
-	rw, openErr := Open(client.ip + client.port)
-	if openErr != nil {
-		log.Println("Failed to connect", openErr)
-		return openErr
-	}
-
-	_, writeErr := rw.WriteString("SYNC\n")
+	_, writeErr := client.conn.WriteString("SYNC\n")
 	if writeErr != nil {
 		log.Println("Failed to write to stream", writeErr)
 		return writeErr
 	}
-	_, writeErr = rw.WriteString(client.name + "\n")
+	_, writeErr = client.conn.WriteString(client.name + "\n")
 	if writeErr != nil {
 		log.Println("Failed to write to stream", writeErr)
 		return writeErr
 	}
-	flushErr := rw.Flush()
+	flushErr := client.conn.Flush()
 	if flushErr != nil {
 		log.Println("Failed to flush", flushErr)
 		return flushErr
 	}
-	response, readErr := rw.ReadString('\n')
+	response, readErr := client.conn.ReadString('\n')
 	if readErr != nil {
 		log.Println("Failed to read response", readErr)
 		return readErr
@@ -125,7 +125,7 @@ func (client *SnekClient) requestGame() error {
 		return errors.New("You are Dead")
 	}
 	var game GameData
-	dec := gob.NewDecoder(rw)
+	dec := gob.NewDecoder(client.conn)
 	err := dec.Decode(&game)
 	if err != nil {
 		log.Println("Failed to sync game")
@@ -135,39 +135,25 @@ func (client *SnekClient) requestGame() error {
 }
 
 func (client *SnekClient) quitGame() error {
-	rw, openErr := Open(client.ip + client.port)
-	if openErr != nil {
-		log.Println("Failed to connect", openErr)
-		return openErr
-	}
 
-	_, writeErr := rw.WriteString("QUIT\n")
+	_, writeErr := client.conn.WriteString("QUIT\n")
 	if writeErr != nil {
 		log.Println("Failed to write to stream", writeErr)
 		return writeErr
 	}
 
-	_, writeErr = rw.WriteString(client.name + "\n")
+	_, writeErr = client.conn.WriteString(client.name + "\n")
 	if writeErr != nil {
 		log.Println("Failed to write to stream", writeErr)
 		return writeErr
 	}
 
-	flushErr := rw.Flush()
+	flushErr := client.conn.Flush()
 	if flushErr != nil {
 		log.Println("Failed to flush", flushErr)
 		return flushErr
 	}
 	return nil
-}
-
-//Open a tcp connection to snek server
-func Open(addr string) (*bufio.ReadWriter, error) {
-	conn, err := net.Dial("tcp", addr)
-	if err != nil {
-		return nil, err
-	}
-	return bufio.NewReadWriter(bufio.NewReader(conn), bufio.NewWriter(conn)), nil
 }
 
 func (client *SnekClient) playSnek() string {
